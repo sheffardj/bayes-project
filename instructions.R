2003024bd0a0a1511d1a76a18d0796472ef15c1f
Dayten Sheffar <sheffardj@gmail.com>
2022-11-10 20:36:18 -0800

    add instructions file for connecting git

---
+++ instructions.R
@@ -0,0 +1,43 @@
+
+### Uncomment the following lines and run them one by one
+### in the RStudio Terminal:
+
+# which git
+## -> should output a folder/directory on 
+## your computer like "/usr/local/bin/git"
+## IF NOT, you need to download GIT before proceeding
+
+# git --version
+## -> should output something like "git version 2.37.2"
+
+# git config  user.name "Your Name"
+# git config  user.email "your@github_email.com"
+
+### Install "usethis" then run the create_from_github command
+# install.packages('usethis')
+# usethis::create_from_github(
+#   "https://github.com/sheffardj/bayes-project.git", #LEAVE THIS!!!
+#   destdir = "~/Documents/UvicTerm1/S564/" ### CHANGE the destination directory!!!
+# )
+
+### Change the name and email, this strictly sets the local user in
+### the file .git/config (a hidden file!!!)
+# git config --local user.name "Dayten Sheffar"
+# git config --local user.email "sheffardj@gmail.com"
+
+## If that all worked as expected (unlikely) then
+## you should be able to pull and push changes via: 
+
+## RStudio > "Tools" > "Version Control" > 
+#### "Commit": opens a window to select your changes and commit 
+#### with a descriptive message
+
+#### "Push": after you commit your changes, you still need to push them
+#### for other people to pull them!
+
+#### "Pull": this pulls the latest state of the branch that reflects other
+#### users having "Pushed" changes to github. This gets you up to date!
+
+
+## more here:
+## https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
