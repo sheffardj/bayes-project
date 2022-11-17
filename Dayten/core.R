83f76d078c38cdf37f953e06671e29f17a0dbac7
Dayten Sheffar <sheffardj@gmail.com>
2022-11-17 14:38:21 -0800

    messing with my file

--- Dayten/core.R
+++ Dayten/core.R
@@ -1,30 +1,22 @@
+## "given" some data
 xx <- rlogitnorm(10000,0.1,2.5)
-pp <- exp(xx) / (1+sum(exp(xx)))
 
+## we decide to fit a logit-normal dist'n
 
+## here is a custom logit-normal pdf
+## but it doesn't draw the legs !
 f <- function(x, mu, sigma) {
   scale <- (1 / (sigma * sqrt(2 * pi)))
-  jacobian <- (1 / (x * (1 - x)))
   expo <- exp(-((logit(x) - mu) ^ 2) / (2 * sigma ^ 2))
+  jacobian <- (1 / (x * (1 - x)))
   return(scale * expo * jacobian)
 }
 
-logit(runif(10000, 0, 1)) %>% hist # takes (0,1) to normal
-f(runif(10000,0,1), 0, 1) %>% hist # takes (0,1) to logit-normal
 
-f(pp, 0.1, 2.5) %>% hist # takes (0,1) to logit-normal
-pp %>% hist
+est_mu <- mean(rnorm(xx, 0.1, 2.5) * dlogitnorm(xx))
 
-
-yy <- rnorm(10000, 0.1, 2.1) #normal
-yy %>% hist
-
-pp <- exp(xx) / (1+sum(exp(xx))) #logit transform
-f(pp, 0.1, 2.1) %>% hist # takes (0,1) to logit-normal
-pp %>% hist
-
-
-
-
-
+hist(xx, breaks=50, probability = T, ylim=c(0,10))
+curve(dlogitnorm(x, 0.1, 2.5), from=0, to=1, add=T, col='blue')
+curve(f(x, 0.1, 2.5), from=0, to=1, add=T, col='blue')
+curve(f(x, est_mu, 2.5), from=0, to=1, add=T, col='red')
 
